942b4cf9a5621e9f071a20812b75c25431fb782f
carissa morrow <carissamorrow@hotmail.com>
2018-10-29 15:05:36 -0600

    newbody

--- main.js
+++ main.js
@@ -21,6 +21,26 @@ let buttons = [
   {
     name: "Center Button",
     function: "Confirms current action"
+  },
+  {
+    name: "Up Volume Button",
+    function: "Raises volume"
+  },
+  {
+    name: "Down Volume Button",
+    function: "Lower volume"
+  },
+  {
+    name: "Channel Up Button",
+    function: "moves channel up one"
+  },
+  {
+    name: "Channel Down Button",
+    function: "moves channel down one"
+  },
+  {
+    name: "Power",
+    function: "turns tv on and off"
   }
 ];
 
