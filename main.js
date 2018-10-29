cf16e7bddfed9bb082499d87fdfe413b72029838
carissa morrow <carissamorrow@hotmail.com>
2018-10-29 14:48:54 -0600

    addbuttons

---
+++ main.js
@@ -0,0 +1,43 @@
+let keypad = {
+  name: "Keypad",
+  arrows: [
+    "left",
+    "right",
+    "up",
+    "down"
+  ]
+}
+
+let buttons = [
+  {
+    name: "Back Button",
+    function: "Goes back"
+  },
+  {
+    name: "Home Button",
+    function: "Goes to home screen"
+  },
+  keypad,
+  {
+    name: "Center Button",
+    function: "Confirms current action"
+  }
+];
+
+let container = {
+  color: "black",
+  texture: "smooth",
+  material: "plastic"
+}
+
+let remote = {
+  buttons,
+  container
+}
+
+console.log(remote.buttons);
+
+// let externals = {
+//   buttons: 12,
+
+// }
